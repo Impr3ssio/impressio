27e18a1546b482aa3deefd4465856c743fc999f6
Impr3ssio <Impr3ssio@gmail.com>
2025-03-28 00:20:57 +0530

    google OAuth fix AGAIN!!

--- config/passport.js
+++ config/passport.js
@@ -23,7 +23,7 @@ module.exports = (passport) => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: 'https://impr3ssio.in/auth/google/callback',
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
